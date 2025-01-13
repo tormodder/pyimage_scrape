8965821ea457c49e017d0497dd2f3958c14d511c
tmod <tormofl@stud.ntnu.no>
2025-01-13 15:13:58 +0100

    add link scraper

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from utils.scraper import get_all_links
 def main():
     target = ""
     output_dir = "output/images"
+    
+    links = get_all_links(target)
+
+    for i in links:
+        print(i)
+
+if __name__ == "__main__":
+    main()
--- utils/scraper.py
+++ utils/scraper.py
@@ -1,6 +1,19 @@
 """
 Module for scraping the web site and gathering URLs
 """
+from bs4 import BeautifulSoup
+import requests
+import sys
 
-def get_all_links():
-    return 0
+def get_all_links(url: str) -> list[str]:
+    re = requests.get(url)
+    if re.status_code != 200:
+        sys.stderr.write("Error getting web page.")
+        exit(-1)
+
+    soup = BeautifulSoup(re.text, 'html.parser')
+
+    return [
+        href for link in soup.find_all("a")
+        if (href := link.get("href")) and url in href and href != url
+    ]
