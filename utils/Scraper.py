948443710e662bf0057f72ab6a7a3a73ba6d4d14
tmod <tormofl@stud.ntnu.no>
2025-02-14 18:48:31 +0100

    add more structure

--- utils/Scraper.py
+++ utils/Scraper.py
@@ -1,81 +1,100 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+
 class Scraper:
     def __init__(self, name):
         self.name = name
+        self._target = ""
+        self._url = ""
         # Add attributes to init
         # self._private = ???
     
     @property
-    def url(self)
+    def url(self):
         return self._url
 
     @property
-        target(self)
-            return self._target
+    def target(self):
+        return self._target
 
     @target.setter
     def target(self, directory:str):
-        self_target = directory
+        self._target = directory
 
     @url.setter
     def url(self, url: str):
-        self._url = value
+        self._url = url
 
     #private methods
 
     ######
     # getting links
     ######
-    def __get_all_links(self, url: str) -> list[str]:
-    re = requests.get(url)
-    if re.status_code != 200:
-        sys.stderr.write("Error getting web page.")
-        exit(-1)
+    def __connect_and_soupify(self, url:str) -> BeautifulSoup:
+        re = requests.get(url)
+        if re.status_code != 200:
+            sys.stderr.write("Error getting web page.")
+            exit(-1)
 
-    soup = BeautifulSoup(re.text, 'html.parser')
+        return BeautifulSoup(re.text, 'html.parser')
 
-    return __get_links_from_same_domain(soup)
 
-    def __get_links_from_same_domain(self, soup: BeautifulSoup) -> list[str]:
-    """
-    Returns only urls in the same domain
-    """
-    return [
-        href for link in soup.find_all("a")
-        if (href := link.get("href")) and (url in href) and (href != url)
-    ]
+    def __get_links_from_same_domain(self, soup: BeautifulSoup, url: str) -> list[str]:
+        """
+        Returns only urls in the same domain
+        """
+        return [
+            href for link in soup.find_all("a")
+            if (href := link.get("href")) and (url in href) and (href != url)
+        ]
     
-    def __get_links_from_different_domain(self, soup: BeautifulSoup) -> list[str]:
-    """
-    filter away links from same domain
-    """
-    return [
-        href for link in soup.find_all("a") 
-        if (href := link.get("href")) and (url not in href)
-    ]
+    def __get_links_to_pictures(self, soup: BeautifulSoup, url: str) -> list[str]:
+        """
+        filter away links from same domain
+        used to get picture links
+        """
+        return [
+            href for link in soup.find_all("a") 
+            if (href := link.get("href")) and (url not in href)
+        ]
 
     #####
     # Page scraping
     ######
-    def __scrape_one_page(self, links: list[str]) -> tuple(str):
-        for link in links:
+    def __scrape_one_page(self, link: str) -> tuple[str, str]:
+         
+
+        return ("", "")
             # This is where the bs4 stuff happens
 
     def __get_image(self, link: str) -> str:
+        return ""
 
     def __get_image_text(self, link: str) -> str:
+        return ""
     
     #TODO: Implement downloader.py
-    def __downloader(self, text_image: tuple(str)):
+    #    def __downloader(self, text_image: tuple(str)):
         # This function should take a tuple: image and text (both strings)
         # and then download the content
         # and put it in the target folder
 
     #public methods
     def scrape(self):
-        (image, text) self.__scrape_one_page(self.__get_all_links(self.url))
+        """
+        Main method of the class
+        Sould call all the other functions
+        """
+        soup = self.__connect_and_soupify(self.url)
 
+        links_to_visit = self.__get_links_from_same_domain(soup, self.url)
 
-    """
+        for link in links_to_visit:
+            (image, text) = self.__scrape_one_page(link)
+
+   
+"""
     TODO
     the program should:
         get all links to the same domain
